06d46c828c66e11b7c17d412507650cd48529ba1
shafiry <yuvalshafir@yahoo.com>
2014-04-05 11:17:17 -0400

    mood reader

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ import requests
 from requests_oauthlib import OAuth1
 import json
 from twkey import *
+from words import *
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/index', methods = ['GET', 'POST'])
@@ -14,7 +15,7 @@ def index():
 	if(form.twitter.data):
 		url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
 		twitter_auth = OAuth1(TW_APIKEY, TW_APISEC, TW_ACCTOK, TW_ACCSEC)
-		twitter_params = {'screen_name': form.twitter.data, 'count':'10'}
+		twitter_params = {'screen_name': form.twitter.data, 'count':'100'}
 		r = requests.get(url, params = twitter_params, auth=twitter_auth)
 		if r.status_code == requests.codes.ok:
 			data = json.loads(r.text)
@@ -22,6 +23,16 @@ def index():
 				posts.append(tweets['text'])
 		else:
 			form.twitter.data = ""
+		mood = 0;
+		for post in posts:
+			words = post.split()
+			for word in words:
+				print word
+				if word.lower() in positive:
+					mood+=1
+				elif word.lower() in negative:
+					mood-=1
+		print mood
 	return render_template("index.html",
 		form = form,
 		posts = posts)
---
+++ app/words.py
@@ -0,0 +1,307 @@
+happy_words = '''Absolutely	Abundant	Accept
+Acclaimed	Accomplishment	Achievement
+Action	Active	Activist
+Acumen	Adjust	Admire
+Adopt	Adorable	Adored
+Adventure	Affirmation	Affirmative
+Affluent	Agree	Airy
+Alive	Alliance	Ally
+Alter	Amaze	Amity
+Animated	Answer	Appreciation
+Approve	Aptitude	Artistic
+Assertive	Astonish	Astounding
+Astute	Attractive	Authentic
+Basic	Beaming	Beautiful
+Believe	Benefactor	Benefit
+Bighearted	Blessed	Bliss
+Bloom	Bountiful	Bounty
+Brave	Bright	Brilliant
+Bubbly	Bunch	Burgeon
+Calm	Care	Celebrate
+Certain	Change	Character
+Charitable	Charming	Cheer
+Cherish	Clarity	Classy
+Clean	Clever	Closeness
+Commend	Companionship	Complete
+Comradeship	Confident	Connect
+Connected	Constant	Content
+Conviction	Copious	Core
+Coupled	Courageous	Creative
+Cuddle	Cultivate	Cure
+Curious	Cute	Dazzling
+Delight	Direct	Discover
+Distinguished	Divine	Donate
+Each Day	Eager	Earnest
+Easy	Ecstasy	Effervescent
+Efficient	Effortless	Electrifying
+Elegance	Embrace	Encompassing
+Encourage	Endorse	Energized
+Energy	Enjoy	Enormous
+Enthuse	Enthusiastic	Entirely
+Essence	Established	Esteem
+Everyday	Everyone	Excited
+Exciting	Exhilarating	Expand
+Explore	Express	Exquisite
+Exultant	Faith	Familiar
+Family	Famous	Feat
+Fit	Flourish	Fortunate
+Fortune	Freedom	Fresh
+Friendship	Full	Funny
+Gather	Generous	Genius
+'''
+
+positive = happy_words.split()
+
+
+sad_words = '''
+A
+abysmal
+adverse
+alarming
+angry
+annoy
+anxious
+apathy
+appalling
+atrocious
+awful
+B
+bad
+banal
+barbed
+belligerent
+bemoan
+beneath
+boring
+broken
+C
+callous
+can't
+clumsy
+coarse
+cold
+cold-hearted
+collapse
+confused
+contradictory
+contrary
+corrosive
+corrupt
+crazy
+creepy
+criminal
+cruel
+cry
+cutting
+D
+dead
+decaying
+damage
+damaging
+dastardly
+deplorable
+depressed
+deprived
+deformed
+D Cont.
+deny
+despicable
+detrimental
+dirty
+disease
+disgusting
+disheveled
+dishonest
+dishonorable
+dismal
+distress
+don't
+dreadful
+dreary
+E
+enraged
+eroding
+evil
+F
+fail
+faulty
+fear
+feeble
+fight
+filthy
+foul
+frighten
+frightful
+G
+gawky
+ghastly
+grave
+greed
+grim
+grimace
+gross
+grotesque
+gruesome
+guilty
+H
+haggard
+hard
+hard-hearted
+harmful
+hate
+hideous
+homely
+horrendous
+horrible
+hostile
+hurt
+hurtful
+I
+icky
+ignore
+ignorant
+ill
+immature
+imperfect
+impossible
+inane
+inelegant
+infernal
+injure
+injurious
+insane
+insidious
+insipid
+J
+jealous
+junky
+L
+lose
+lousy
+lumpy
+M
+malicious
+mean
+menacing
+messy
+misshapen
+missing
+misunderstood
+moan
+moldy
+monstrous
+N
+naive
+nasty
+naughty
+negate
+negative
+never
+no
+nobody
+nondescript
+nonsense
+not
+noxious
+O
+objectionable
+odious
+offensive
+old
+oppressive
+P
+pain
+perturb
+pessimistic
+petty
+plain
+poisonous
+poor
+prejudice
+Q
+questionable
+quirky
+quit
+R
+reject
+renege
+repellant
+reptilian
+repulsive
+repugnant
+revenge
+revolting
+rocky
+rotten
+rude
+ruthless
+S
+sad
+savage
+scare
+scary
+scream
+severe
+shoddy
+shocking
+sick
+sickening
+sinister
+slimy
+smelly
+sobbing
+sorry
+spiteful
+sticky
+stinky
+stormy
+stressful
+stuck
+stupid
+substandard
+suspect
+suspicious
+T
+tense
+terrible
+terrifying
+threatening
+U
+ugly
+undermine
+unfair
+unfavorable
+unhappy
+unhealthy
+unjust
+unlucky
+unpleasant
+upset
+unsatisfactory
+unsightly
+untoward
+unwanted
+unwelcome
+unwholesome
+unwieldy
+unwise
+upset
+vice
+vicious
+vile
+villainous
+vindictive
+wary
+weary
+wicked
+woeful
+worthless
+wound
+yell
+yucky
+zero
+'''
+
+negative = sad_words.split()
+
+
